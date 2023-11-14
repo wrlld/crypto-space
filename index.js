8e6a41218c3edcb86eb26a743b9ec0f6285d0875
wrlld01~1~ <ndabaigodwin@gmail.com>
2023-11-14 15:35:41 +0100

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,154 @@
-function isIsogram(str){
-    // to lowercase    
-    str = str.toLowerCase();
-    // split the lowercase
-    let isogram = str.split("");
-    let sortisogram = isogram.slice().sort();
+// function findNextSquare(sq){
+//    const sQrt = Math.sqrt(sq)
+   
+//    if(Number.isInteger(sQrt)){
+//         const nextSqrt = sQrt / 3
+
+//         const nextPerfectSquare = nextSqrt * 1
+
+//         return nextPerfectSquare
+//    }
+
+//    return -1;
+// }
+
+// console.log(findNextSquare(81));
+
+
+// function getCount(str) {
+//     var string = str.toLowerCase();
+//     let array = string.split('')
+//     const vowelCount = array.filter(count => /[aeiou]/.test(count)).length;
     
-    for(let i = 0; 1 < isogram.length; i++){
-        if(sortisogram[i + 1] == sortisogram[i]){ 
-            return false;
-        }else{
-            return true;
-        }
+//     return vowelCount;
+//   }
+  
+//   console.log(getCount("abracadabra"));
+//console.log(findNextSquare(121));
+function toCamelCase(str){
+
+    const filterStr = str.replace(/[-_]/gi, " ")
     
+    let arr = filterStr.split(" ")
+
+    let result = []
+    let currentWord = ""
+
+    
+
+    for( let i = 0; i < arr.length; i++){
+        // get the elements and push into a new array
+        currentWord = arr[i]
+        console.log(currentWord);
+        // get the elements greater the first
+        if(i > 0){
+           
+            let newWord = arr[i].replace(arr[i][0], arr[i][0].toUpperCase())
+
+            currentWord = newWord
+
+        }   
+
+        
+        result.push(currentWord)
+        
     }
-        console.log(sortisogram);
-    }
-    console.log(isIsogram("listen"));
+
+    return result.join("")
+    console.log(currentWord);
+}
+
+console.log(toCamelCase("the-stealth-warrior"));
+
+
+// const str = 'The quick brown fox jumps over the lazy dog.';
+// const chars = str.split(' ');
+// console.log(chars[8]); gonna come back.
+
+// function isIsogram(str){
+//     // to lowercase    
+//     str = str.toLowerCase();
+//     // split the lowercase
+//     let isogram = str.split("");
+//     let sortisogram = isogram.slice().sort();
+    
+//     for(let i = 0; 1 < isogram.length; i++){
+//         if(sortisogram[i + 1] == sortisogram[i]){ 
+//             return false;
+//         }else{
+//             return true;
+//         }
+    
+//     }
+//         console.log(sortisogram);
+//     }
+//     console.log(isIsogram("listen"));
     
     
-    // solution 2.
-    function FilterNumbers(numbers){
-        const evenNumbers = numbers.filter((number) => number % 2 == 0);
-        const oddNumbers = numbers.filter((number) => number % 2 !== 0);
-        return{evenNumbers, oddNumbers};
-    }
+//     // solution 2.
+//     function FilterNumbers(numbers){
+//         const evenNumbers = numbers.filter((number) => number % 2 == 0);
+//         const oddNumbers = numbers.filter((number) => number % 2 !== 0);
+//         return{evenNumbers, oddNumbers};
+//     }
     
-    const numbers = [2, 10, 5, 7, 9, 100, 23, 17, 20];
-    const {evenNumbers, oddNumbers} = FilterNumbers (numbers);
-    console.log([evenNumbers, oddNumbers]);
+//     const numbers = [2, 10, 5, 7, 9, 100, 23, 17, 20];
+//     const {evenNumbers, oddNumbers} = FilterNumbers (numbers);
+//     console.log([evenNumbers, oddNumbers]);
+
+//     var power = function(base, exponent) {
+//         var result = 1;
+//         for (var count = 0; count < base; count++)
+//         result *= exponent;
+//         console.log(result);
+//         };
+//         console.log(power(2, 10));
+        
+
+
+//         var power = function(base , squareroot) {
+//             let result = 1;
+//             for(let i = 0; i < base; i++) {
+//                 return base /= squareroot;
+//                 return result; 
+//             }
+//         }
+//         console.log(power(144, 12));
+
+//         var x = "outside";
+//         let f1 = function() {
+//             var x = "inside.f1";
+//         }
+//         f1();
+//         console.log(x);
+
+//         var f2 = function(x) {
+//             console.log(x);
+//         }
+//         f2("inside f2");
+        
+//         var hello = "morning";
+
+//         function greet(who) {
+//             console.log("Hello " + who);
+            
+//             }
+//             greet("Harry");
+//             console.log(hello);
+//             console.log("Bye");
+
+
+            /*function chicken() {
+                return egg();
+                }
+                function egg() {
+                return chicken();
+                }
+                console.log(chicken() + " came first.");
+
+                function randomNumber(){
+                    randomNumber = [13, "hey", "big head", 17]
+                }
+                console.log(randomNumber[1]);*/
+
+                    
--- index1.js
+++ index1.js
@@ -1,155 +1,68 @@
-/* var myStrings = 'i\'m a "fun godwin" string';
-console.log(myStrings.indexOf("godwin"));
-var str = "he is the reason i lied"
-console.log(str);
-var sts = str.slice(6,16);
-console.log(sts);
+/*function selectNumber(){
+    const array = [13, "hello", "thick", 17];
+    console.log(array.length)
+    const random = Math.floor(Math.random() * array.length);
+    console.log(array[random]);
+}    
+selectNumber();
 
-var myString = "i want to be a front-end developer";
-console.log(myString.toUpperCase()); 
+var power = function(base, exponent) {
+    var result = 2;
+    for (var count = 0; count < exponent; count++)
+    result *= base;
+    return result;
+    };
+    console.log(power(2, 10));
 
-let word = "rthyms";
+    const array = ["boy", true, 13, "sapa"];
 
-if( word.indexOf("a") !== -1  || word.indexOf("e") !== -1 || word.indexOf("i") !== -1 || word.indexOf("o") !== -1 || word.indexOf("u") !== -1){
+    const array1 = array.pop();
+    console.log(array)
+    console.log(array1);
+    const array2 = array.push("sapa");
+    console.log(array);
+    console.log(array2);*/
 
-    console.log("it contains a vowel");
-
-}else{
-
-    console.log("it has no vowel")
-
-
-}*/
-/*var tags = "rice, beans, garri, oil, yam"
-
-var myCar = new Object();
-
-myCar.maxspeed = 50;
-
-myCar.driver = "Godwin";
-
-console.log(myCar.maxspeed);
-
-console.log(myCar.driver);
-
-var array = [];
-array[0] = 5;
-array[1] = "gift";
-array[2] = "jehovah";
-console.log(array);
-console.log(array[2]);
+    // const x = "outside";
 
 
-myCar.drive = function(){ console.log("now driving");};
-myCar.drive();
+    // var f1 = function() {
+    // let x = "inside f1";
+    // let fig = 13;
+    // console.log(fig);
+    // };
+    // f1();
+    // console.log(f1());
 
-var myCar2 = {maxspeed: 70, driver: "Godwin", drive: function(){ console.log("now driving again");}};
-console.log(myCar2.maxspeed);
-myCar2.drive();
-myCar2.toUppercase = function(){ console.log("A-Z");};
-myCar2.toUppercase();
+    // //dont really understand
+    // function greet(who) {
+    //     return who;
+    //     }
+    //     console.log("Hello " + greet("harry"));
+    //     console.log(greet("harry"));
 
-
-function getAverage  (a,b){
-
-    var average = (a + b) /2;
-    console.log(average);
-
-}
-getAverage(3,5);
-var fast = "come";
-var fast = "bro's";
-console.log(fast); 
-
-let men = "boy";
-men = "girl"
-console.log(men)
-
-var mystring ="product";
-
-for(let myString = "#"; myString.length < 8; myString += "#"){
-
-    console.log(myString);
-
-}*/
+       
+    //     function who(who) {
+    //         console.log("i love craze " + who);
+    //         }
+    //         who("people");  
 
 
-for(let myNumber = 1; myNumber <= 100; myNumber++){
+    //         function chicken() {
+    //             return egg;
+    //             }
+    //             function egg() {
+    //             return chicken;
+                
+    //             }
+    //             console.log(egg() + " came first.");
 
-    if(myNumber % 3 == 0 && myNumber % 5 == 0){
-        console.log("FizzBuzz");
-    }else if(myNumber % 3 == 0){
-        console.log("Fizz");
-    }else if(myNumber % 5 == 0){
-        console.log("Buzz");
-    }else{
-        console.log(myNumber);
-    }
-    
-}  
-
-/*function anagram(str1, str2) {
-    if (str1.length !== str2.length) {
-        return false;
-    }
-    
-    const sortStr1 = str1.split("").sort().join("");
-
-    const sortStr2 = str2.split("").sort().join("");
-    
-    return sortStr1 == sortStr2
-
-}
-
-console.log(anagram("listen","silent"));
+    /*function func(x) { var x = 5;
+    return x; }
+     console.log(x)
+     ;*/
 
 
+     //
 
-
-
-function longestWord(sentence){
-    const words = sentence.split(" ");
-    let longWord = "";
-    for (let i = 0; i < words.length; i++){
-
-        const word = words[i].replace(/[.,!?]/g, '');
-        if(word.length > longWord.length){
-            longWord = word;
-        }
-
-    }
-    return longWord;
-}
-console.log(longestWord("Godwin has a big head"));*/
-
-
-function isIsogram(str){
-// to lowercase    
-str = str.toLowerCase();
-// split the lowercase
-let isogram = str.split("");
-let sortisogram = isogram.slice().sort();
-
-for(let i = 0; 1 < isogram.length; i++){
-    if(sortisogram[i + 1] == sortisogram[i]){ 
-        return false;
-    }else{
-        return true;
-    }
-
-}
-    console.log(sortisogram);
-}
-console.log(isIsogram("listen"));
-
-
-// solution 2.
-function FilterNumbers(numbers){
-    const evenNumbers = numbers.filter((number) => number % 2 == 0);
-    const oddNumbers = numbers.filter((number) => number % 2 !== 0);
-    return{evenNumbers, oddNumbers};
-}
-
-const numbers = [2, 10, 5, 7, 9, 100, 23, 17, 20];
-const {evenNumbers, oddNumbers} = FilterNumbers (numbers);
-console.log([evenNumbers, oddNumbers]);
+                
